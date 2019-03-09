806932727451af7e716eb13b557ded71210bc46e
Sandesh Karki <karkisandesh@hotmail.com>
2019-03-09 22:55:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,11 @@
 class Triangle
-  # write code here
+  def initialize(length, breadth, height)
+    @length = length 
+    @breadth = breadth 
+    @height = height 
+  end 
+  
+  def kind 
+end
+
 end
