1fa16b9583302629ec0bd1391b138b9ef14c1e23
Sandesh Karki <karkisandesh@hotmail.com>
2019-03-10 00:06:18 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,15 +1,24 @@
 class Triangle
-  
-  attr_reader :length1, :length2, :length3
-  def initialize(length1, length2, length3)
-    @length1 = length1 
-    @length2 = length2 
-    @length3 = length3
-  end 
-  
-  def kind
-    
+ def initialize(side_1,side_2,side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+  end
+
+  def kind()
+    if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
       raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+      raise TriangleError
+    else
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
+    end
   end
 
 class TriangleError < StandardError 
