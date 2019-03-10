6c367d8f9d243b6bd7874c8535a5c0f18ae42dbe
Sandesh Karki <karkisandesh@hotmail.com>
2019-03-10 00:07:53 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
   end
 
   def kind()
-    if (@side_1 * @side_2 * @side_3 = 0) || ((@side_1 + @side_2 <= @side_3) || (@side_1 + @side_3 <= @side_2) || (@side_2 + @side_3 <= @side_1))
+    if (@side_1 * @side_2 * @side_3 == 0) || ((@side_1 + @side_2 <= @side_3) || (@side_1 + @side_3 <= @side_2) || (@side_2 + @side_3 <= @side_1))
       raise TriangleError
     else
       if (@side_1 == @side_2) && (@side_2 == @side_3)
